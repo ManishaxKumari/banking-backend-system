9cf67c80a275078d98f7211b097e4c405e33e8e8
Manisha <manisha857522@gmail.com>
2026-07-16 12:00:15 +0530

    Add CORS middleware and update package dependencies

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ const cookieParser=require("cookie-parser")
 const app = express()
 
 app.use(express.json()) 
-app.use(cookieParser()) 
+app.use(cookieParser())
+
+const cors = require("cors")
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true 
+}))
 /**
  * Routes 
  */
